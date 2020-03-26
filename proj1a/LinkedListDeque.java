8e26ab0cd846908600da06b8d607975bb9d42e6e
Samuel-Sorial <eng.samuel.maged@gmail.com>
2020-03-26 22:57:21 +0200

    addFirst Method done

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+public class LinkedListDeque<T> {
 
-public class LinkedListDeque {
+    private TNode sentinel;
+    private int size;
+
+    public class TNode{
+        public TNode prev;
+        public T item;
+        public TNode next;
+
+        public TNode(TNode prev, T item, TNode next) {
+            this.prev = prev;
+            this.item = item;
+            this.next = next;
+            size = 0;
+        }
+    }
+
+    public LinkedListDeque(){
+        sentinel = new TNode(null,null,null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+        size=0;
+    }
+
+    public void addFirst(T item){
+        TNode added = new TNode(sentinel ,item, sentinel.next);
+        sentinel.next.prev = added;
+        sentinel.next = added;
+        size++;
+    }
+
+
 }
