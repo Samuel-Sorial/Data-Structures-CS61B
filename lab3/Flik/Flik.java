27760da2c222e09bc6e8170e2d0f9e9e056a5a9e
Samuel-Sorial <eng.samuel.maged@gmail.com>
2020-03-28 19:41:05 +0200

    Lab 3 done with debugging fliker and passing the required tests

--- lab3/Flik/Flik.java
+++ lab3/Flik/Flik.java
@@ -1,6 +1,11 @@
 /** An Integer tester created by Flik Enterprises. */
 public class Flik {
+    /*
+    The bug is that Integer a and b is not primitive, they are objects and have the same address until 127,
+    at 128 they got different addresses and that's why it breaks at the 128,
+    here's the correct method that passes the tests.
+     */
     public static boolean isSameNumber(Integer a, Integer b) {
-        return a == b;
+        return a.intValue() == b.intValue();
     }
 }
--- lab3/Flik/HorribleSteve.java
+++ lab3/Flik/HorribleSteve.java
@@ -1,12 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 public class HorribleSteve {
-    public static void main(String [] args) throws Exception {
+    @Test
+    public void testFlik() {
         int i = 0;
-        for (int j = 0; i < 500; ++i, ++j) {
-            if (!Flik.isSameNumber(i, j)) {
-                throw new Exception(
-                        String.format("i:%d not same as j:%d ??", i, j));
-            }
+        for (int j  = 0; i  < 500; ++i, ++j) {
+            assertTrue("They are not equal", Flik.isSameNumber(i, j));
         }
-        System.out.println("i is " + i);
     }
 }
